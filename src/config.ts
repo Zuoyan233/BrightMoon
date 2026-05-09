cede22af4c75a870e9ebaf396b02dfd03e959de1
Zuoyan233 <zuoxiju@outlook.com>
2026-05-09 13:41:56 +0800

    移除 FooterConfig.html 使用配置中的 customHtml

--- src/config.ts
+++ src/config.ts
@@ -277,8 +277,8 @@ export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 		interval: 6, // 轮播间隔时间（秒）
 	},
 	zIndex: -1, // 层级，确保壁纸在背景层
-	opacity: 0.8, // 壁纸透明度
-	blur: 1, // 背景模糊程度
+	opacity: 1, // 壁纸透明度
+	blur: 8, // 背景模糊程度
 };
 
 export const navBarConfig: NavBarConfig = {
@@ -498,10 +498,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 
 export const footerConfig: FooterConfig = {
 	enable: true, // 是否启用Footer HTML注入功能
-	customHtml: "", // HTML格式的自定义页脚信息，例如备案号等，默认留空
-	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
-	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
-	// FooterConfig.html 可能会在未来的某个版本弃用
+	customHtml: "本平台为个人博客站，仅限用于学习和研究目的", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 };
 
 /**
