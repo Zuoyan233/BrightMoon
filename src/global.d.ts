770ab99bd7f38e94e2a47686a41db798a8f4ae46
Zuoyan233 <zuoxiju@outlook.com>
2026-08-20 02:12:50 +0800

    修复加密文章中代码块样式问题

--- src/global.d.ts
+++ src/global.d.ts
@@ -39,11 +39,25 @@ declare global {
 		siteConfig: Record<string, unknown>;
 		CryptoJS?: {
 			AES: {
-				encrypt: (message: string, password: string) => { toString: () => string };
-				decrypt: (ciphertext: string, password: string) => { toString: (encoder?: { parse: (s: string) => unknown; stringify: (w: unknown) => string }) => string };
+				encrypt: (
+					message: string,
+					password: string,
+				) => { toString: () => string };
+				decrypt: (
+					ciphertext: string,
+					password: string,
+				) => {
+					toString: (encoder?: {
+						parse: (s: string) => unknown;
+						stringify: (w: unknown) => string;
+					}) => string;
+				};
 			};
 			enc: {
-				Utf8: { parse: (s: string) => unknown; stringify: (w: unknown) => string };
+				Utf8: {
+					parse: (s: string) => unknown;
+					stringify: (w: unknown) => string;
+				};
 			};
 		};
 	}
